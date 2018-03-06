f32dd524a464b27e8e7e1a3c140744310da5d403
Lucas Tan <lucas.j.tan@gmail.com>
2018-03-05 20:42:05 -0800

    Fixed Footer, Changed 404 Page

---
+++ footer.js
@@ -0,0 +1,1 @@
+document.getElementById("footer").innerHTML = "<span style='background-color: black; height:75px; width: 100%; position: absolute; bottom: -1; right: 0; left: 0; margin-top: -3em;'><div style='position:inherit; transform: translate(0%,120%); margin-left:10px; z-index: 10; color: white; font-size: 100%; font-family: Avenir;'>© Copyright Tangrams Apps 2018.</div></span>";
